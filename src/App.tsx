829e223d9f0da247e2b4bed066ac83006aaa7b34
Amir <amir.saeed.ali92@gmail.com>
2021-01-21 01:59:44 +0500

    level button fixed

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { getQuizzDetails } from "./services/quizzQuestion";
+import { QuestionCard } from "./Component/QuestionCard";
+import { QuestionType } from "./type/quiz_type";
+import Modal from "./Component/Modal";
+function App() {
+    let [quiz, setQuiz] = useState<QuestionType[]>([]);
+    let [currentStep, setCurrentStep] = useState(0);
+    let [score, setScore] = useState(0);
+    let [showResult, setShowResult] = useState(false);
+
+    useEffect(() => {
+        async function fetchData() {
+            const questions: QuestionType[] = await getQuizzDetails(5, "easy");
+            console.log(questions);
+            setQuiz(questions);
+            console.log("this", quiz);
+        }
+        fetchData();
+    }, []);
+    if (!quiz.length) {
+        return <h3>loading..</h3>;
+    }
+
+    const ReloadFunc = (e: React.FormEvent<EventTarget>, userAns: string) => {
+        e.preventDefault();
+
+        const currentQuestions: QuestionType = quiz[currentStep];
+
+        console.log("w", currentQuestions.correct_answer);
+
+        if (userAns === currentQuestions.correct_answer) {
+            setScore(++score);
+        }
+
+        if (currentStep !== quiz.length - 1) setCurrentStep(++currentStep);
+        else {
+            setShowResult(true);
+        }
+    };
+    if (showResult) {
+        return (
+            <div className="question-container result-container">
+                <h2>Result</h2>
+
+                <p className="result-text">
+                    You final score is
+                    <b> {score}</b> out of <b>{quiz.length}</b>
+                </p>
+            </div>
+        );
+    }
+    return (
+        <>
+            <div className="App">
+                <QuestionCard
+                    options={quiz[currentStep].option}
+                    question={quiz[currentStep].question}
+                    callback={ReloadFunc}
+                />
+            </div>
+            <div>
+                <Modal />
+            </div>
+        </>
+    );
+}
+export default App;
---
+++ src/Component/Button.tsx
@@ -0,0 +1,98 @@
+import React, { useState, useRef, useEffect } from "react";
+import Button from "@material-ui/core/Button";
+
+function ButtonGroups(props: any) {
+  const [level, setLevel] = useState("");
+  const [click, setClick] = useState(false);
+  const firstBtn = useRef<HTMLButtonElement>(null);
+  const secondBtn = useRef<HTMLButtonElement>(null);
+  const thirdBtn = useRef<HTMLButtonElement>(null);
+
+  const clickBtn = (x: any) => {
+    setLevel(x.current?.value);
+  };
+
+  return (
+    <>
+      <div className="level">
+        <p>{props.name}</p>
+        <Button
+          ref={firstBtn}
+          size="small"
+          variant="contained"
+          color="secondary"
+          value={props.btn1}
+          onClick={() => clickBtn(firstBtn)}
+        >
+          {props.btn1}
+        </Button>
+        <Button
+          ref={secondBtn}
+          size="small"
+          variant="contained"
+          color="secondary"
+          value={props.btn2}
+          onClick={() => clickBtn(secondBtn)}
+        >
+          {props.btn2}
+        </Button>
+        <Button
+          size="small"
+          variant="contained"
+          color="secondary"
+          value={props.btn3}
+          ref={thirdBtn}
+          onClick={() => clickBtn(thirdBtn)}
+        >
+          {props.btn3}
+        </Button>
+      </div>
+    </>
+  );
+}
+// function ButtonGroups1(props: any) {
+//     const [question, setQuestions] = useState("");
+//     const Btn4 = (e: any) => {
+//         console.log(e.target.name);
+//         setQuestions(e.target.name);
+//     };
+//     const Btn5 = (e: any) => {
+//         alert("workig5");
+//         setQuestions(e.target.name);
+//     };
+//     const Btn6 = (e: any) => {
+//         alert("workig6");
+//         setQuestions(e.target.name);
+//     };
+//     return (
+//         <div className="level">
+//             <p>{props.name}</p>
+//             <Button
+//                 onClick={Btn4}
+//                 size="small"
+//                 variant="contained"
+//                 color="secondary"
+//                 name="5"
+//             >
+//                 {props.btn1}
+//             </Button>
+//             <Button
+//                 onClick={Btn5}
+//                 size="small"
+//                 variant="contained"
+//                 color="secondary"
+//             >
+//                 {props.btn2}
+//             </Button>
+//             <Button
+//                 onClick={Btn6}
+//                 size="small"
+//                 variant="contained"
+//                 color="secondary"
+//             >
+//                 {props.btn3}
+//             </Button>
+//         </div>
+//     );
+// }
+export default ButtonGroups;
